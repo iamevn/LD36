4cdffa887542f6666ffe50e8b7a65d06c94a6339
iamevn <iamevn@users.noreply.github.com>
2016-08-27 14:44:20 -0400

    undo jokes, add bouncing

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
 require "love"
 flux = require "flux"
 
-local state = 0
-local timeout = 0
 local sprites = {}
 local rockdata = {
     launched = false,
@@ -15,73 +13,69 @@ local rockdata = {
     rotvel = 0, --full rotations per second
     rot = 0, --in radians
     scl = 0.25,
-    friction = 10
+    friction = 10,
+    gravity = 10,
+    mass = 50,
+    bouncedamp = 2
 }
 
 function launch()
     rockdata.launched = true
-    rockdata.xvel = 100
 end
 
 function love.load()
     sprites.rock = love.graphics.newImage("res/rock.png")
     sprites.background = love.graphics.newImage("res/background.png")
+    sprites.hill = love.graphics.newImage("res/hill.png")
+    sprites.ramp = love.graphics.newImage("res/ramp.png")
 end
 
 function love.update(dt)
     flux.update(dt)
-    if state == 0 then
-	local r = rockdata
+    local r = rockdata
 
-	r.rot = (r.rot + r.rotvel * 2 * math.pi * dt) % (2 * math.pi)
-	r.xpos = r.xpos + r.xvel * dt
-	r.ypos = r.ypos + r.yvel * dt
+    r.rot = (r.rot + r.rotvel * 2 * math.pi * dt) % (2 * math.pi)
+    r.xpos = r.xpos + r.xvel * dt
 
-	if r.ypos < 10 then
-	    r.xvel = r.xvel - dt * r.friction
-	    if r.xvel < 0 then r.xvel = 0 end
-	end
+    if r.ypos < 10 then
+	r.xvel = r.xvel - dt * r.friction
+	if r.xvel < 0 then r.xvel = 0 end
+    end
 
-	r.rotvel = r.xvel/64
+    r.rotvel = r.xvel/64
 
-	if r.launched and r.xvel == 0 and r.yvel == 0 then
-	    timeout = timeout + dt
-	end
-	if timeout > 1 then
-	    state = 1
-	    timeout = 0
-	end
-    elseif state <= 3 then
-	-- Yeah, it's one of these games
-	-- I know
-	--
-	-- It's a pretty ancient genre isn't it?
-	timeout = timeout + dt
-	if timeout > 3 then
-	    state = state + 1
-	    timeout = 0
+    local ERR = 0.1
+    if r.launched then
+	if r.ypos < 0 then
+	    r.yvel = -r.yvel * 1/r.bouncedamp
+	    r.ypos = 0
+	    if r.yvel < 0 then
+		r.ypos = 0
+		r.yvel = 0
+	    end
+	elseif r.ypos < 0+ERR and r.yvel > -ERR and r.yvel < ERR then
+	    r.yvel = 0
+	    r.ypos = 0
+	else
+	    r.yvel = r.yvel - r.gravity * r.mass * dt
+	    r.ypos = r.ypos + r.yvel * dt
 	end
     end
 end
 
 function love.draw()
-    if state == 0 then
-	love.graphics.clear(132, 209, 227, 255) --sky color
-	love.graphics.draw(sprites.background, -rockdata.xpos, 0)
-	local r = rockdata
-	love.graphics.printf("Distance: "..math.floor(r.xpos / 100).."\nHeight: "..math.floor(r.ypos), 20, 20, 760)
-	love.graphics.draw(sprites.rock, r.xoffset, (love.graphics.getHeight() - r.ypos - r.yoffset), r.rot, r.scl, r.scl, sprites.rock:getWidth()/2, sprites.rock:getHeight()/2)
-    else
-	local str = "Yeah it's one of these games"
-	if state > 1 then str = str.."\nI know" end
-	if state > 2 then str = str.."\n\nIt's a pretty ancient genre isn't it?" end
-	love.graphics.clear(0, 0, 0, 255) --black
-	love.graphics.printf(str, 800/2, 250, 760)
-    end
+    love.graphics.clear(132, 209, 227, 255) --sky color
+    love.graphics.draw(sprites.background, -(rockdata.xpos%sprites.background:getWidth()), 0)
+    love.graphics.draw(sprites.background, -(rockdata.xpos%sprites.background:getWidth())+sprites.background:getWidth(), 0)
+    love.graphics.draw(sprites.hill, -rockdata.xpos, 0)
+    love.graphics.draw(sprites.ramp, -rockdata.xpos, 0)
+    local r = rockdata
+    love.graphics.printf("Distance: "..math.floor(r.xpos / 100 + 0.5).."\nHeight: "..math.floor(r.ypos + 0.5), 20, 20, 760)
+    love.graphics.draw(sprites.rock, r.xoffset, (love.graphics.getHeight() - r.ypos - r.yoffset), r.rot, r.scl, r.scl, sprites.rock:getWidth()/2, sprites.rock:getHeight()/2)
 end
 
 function love.keyreleased(key)
-    if (not rockdata.launched) and key == 'space' then
-	flux.to(rockdata, 3, {xpos=280, ypos=0, xvel=100, xoffset=150}):ease("quadin"):oncomplete(launch)
+    if (not rockdata.launched) and rockdata.xpos == 0 and key == 'space' then
+	flux.to(rockdata, 3, {xpos=280, ypos=0, xvel=100, xoffset=150}):ease("quadin"):after(0.5, {xpos=280+(100/2), ypos=50, yvel=100}):oncomplete(launch)
     end
 end
