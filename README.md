af15e6b24e41e9c87f31b54ad374ae5c3576b609
iamevn <iamevn@users.noreply.github.com>
2016-08-27 15:34:11 -0400

    slight background changes and a title

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Ludum Dare 36 Project, Ancient Technology
+# The Human Drive For Innovation In The Realm Of Science (Ludum Dare 36 Ancient Technology project)
 
 You'll need [LÖVE](https://love2d.org)
--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-    t.window.title = "Ancient Ludum Dare"
+    t.window.title = "The Human Drive For Innovation In The Realm Of Science"
     t.window.width = 800
     t.window.height = 600
 end
